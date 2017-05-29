10e60809138d6576f496d209b85428270ab3bb87
vitkyk <pashcin2000@mail.ru>
2017-05-29 23:19:51 +0600

    Add native-form and validated-input

--- app/controllers/test.js
+++ app/controllers/test.js
@@ -2,6 +2,35 @@ import Ember from 'ember';
 import TestValidations from '../validations/test';
 
 export default Ember.Controller.extend({
+  formDescription: {
+    header: 'Profile form',
+    elementId: '',
+    saveButtonLabel: 'Save Profile',
+    cancelButtonLabel: 'Cancel',
+
+    nickname: 'name',
+    language: 'germany',
+    checkbox: true,
+    descriptions: [
+      {
+        index: 'nickname',
+        componentType: 'text',
+        type: 'text',
+        value: 'some',
+      },
+      {
+        index: 'language',
+        componentType: 'select',
+        value: 'germany',
+        options: ['france', 'germany', 'spain'],
+      },
+      {
+        index: 'checkbox',
+        componentType: 'checkbox',
+        type: 'checkbox',
+      },
+    ],
+  },
   TestValidations,
   actions: {
     submit(changeset) {
--- app/validations/test.js
+++ app/validations/test.js
@@ -3,6 +3,7 @@ import {
 } from 'ember-changeset-validations/validators';
 
 export default {
+  nickname: [validatePresence(true)],
   name: [validatePresence(true)],
   description: [validatePresence(true)],
 };
