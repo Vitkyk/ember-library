a4963b6f1e182021e3bacc1ee6b265c02976e882
vitkyk <pashcin2000@mail.ru>
2017-06-04 17:26:37 +0600

    Add options for pagination

--- app/components/admin-table.js
+++ app/components/admin-table.js
@@ -1,4 +1,31 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  itemsOnPageOptions: [
+    {
+      value: 5,
+      disabled: true,
+    },
+    {
+      value: 10,
+      disabled: false,
+    },
+    {
+      value: 20,
+      disabled: false,
+    },
+  ],
+  itemsOnPage: 5,
+  numberOfPages: Ember.computed('model.[]', 'itemsOnPage', function() {
+    return Math.floor(this.get('model').get('content').length / this.get('itemsOnPage'));
+  }),
+ actions:{
+    selectNumberOfItemsOnPage(value) {
+      this.set('itemsOnPage', value);
+      const itemsOnPageOptions = this.get('itemsOnPageOptions');
+      for (let i = 0; i < itemsOnPageOptions.length; i++ ) {
+        Ember.set(itemsOnPageOptions[i], 'disabled', value === itemsOnPageOptions[i].value);
+      }
+    }
+ }
 });
